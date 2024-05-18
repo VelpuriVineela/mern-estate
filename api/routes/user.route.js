96b88c5013039d640b154603f06ad0c8b301ebdc
velpuri vineela <vineelavelpuri@gmail.com>
2024-05-18 16:56:22 +0530

    create get user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   deleteUser,
   test,
   updateUser,
+  getUserListings,
 } from "../controllers/user.controllers.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -12,5 +13,6 @@ router.get("/test", test);
 // token verification and then updation
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
