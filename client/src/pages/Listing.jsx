318f3ee41e137a7b7feec177441c1a0f7e80a2b8
velpuri vineela <vineelavelpuri@gmail.com>
2024-05-21 12:22:38 +0530

    add image slier to the listing page

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+// swiper is installed for the sliding of images
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+
+export default function Listing() {
+  SwiperCore.use([Navigation]);
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const params = useParams();
+  useEffect(() => {
+    // fetching data for listing page
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    fetchListing();
+  }, [params.listingId]);
+
+  console.log(loading);
+  return (
+    <main>
+      {/* for loading effect before page load */}
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+
+      {/* for any error happen to get the page */}
+      {error && (
+        <p className="text-center my-7 text-2xl">Something went wrong!...</p>
+      )}
+
+      {/* if it is error free and loading is completed page must appears */}
+      {listing && !loading && !error && (
+        <>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      )}
+    </main>
+  );
+}
